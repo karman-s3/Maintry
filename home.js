216601251b487900adad9cb764deb96ef1d91402
Karman <ksidhu2806@gmail.com>
2026-07-12 23:17:28 -0400

    Update home.js

--- home.js
+++ home.js
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (logoutBtn) {
         logoutBtn.addEventListener('click', () => {
             localStorage.removeItem('token');
-            window.location.href = 'landing.html';
+            window.location.href = 'index.html';
         });
     }
 
